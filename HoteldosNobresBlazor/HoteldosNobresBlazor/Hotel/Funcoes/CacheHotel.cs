94df1a1d7fa2bc1e30f77d8fd0350ee8991e7104
Fabio Nobre <fabiohcnobre@hotmail.com>
2024-04-01 19:52:21 -0300

    #0 - Ajuste

--- HoteldosNobresBlazor/HoteldosNobresBlazor/Hotel/Funcoes/CacheHotel.cs
+++ HoteldosNobresBlazor/HoteldosNobresBlazor/Hotel/Funcoes/CacheHotel.cs
@@ -457,14 +457,28 @@ namespace HoteldosNobresBlazor.Funcoes
                         && ( reserva.Origem.ToUpper().Contains("AIRBNB") || reserva.Origem.ToUpper().Contains("BOOKING.COM")))
                     {
                         bool voltar = false;
+
+
                         if (string.IsNullOrEmpty(reserva.Cpf))
                         {
                             voltar = true;
                             logSistema.Log += FunctionWhatsApp.postMensagem("553537150180", "Por favor informe seu CPF na reserva do " + reserva.NomeHospede).Result;
 
-                            logSistema.Log += FunctionWhatsApp.postMensagem(reserva.ProxyCelular, "Por favor informe seu CPF na reserva. Na recepçao e solicite para preencher no sistema.").Result;
+                            logSistema.Log += FunctionWhatsApp.postMensagem(reserva.ProxyCelular, "Por favor informe seu CPF para reserva.").Result;
+                            logSistema.Log += FunctionWhatsApp.postMensagem(reserva.ProxyCelular, " Na recepçao e solicite para preencher no sistema.").Result;
                             logSistema.Log += FunctionWhatsApp.postMensageFlowCPF(reserva.ProxyCelular).Result;
                         }
+
+                        if(reserva.Cpf != null && !ValidarCPF(reserva.Cpf))
+                        {
+                            voltar = true;
+                            logSistema.Log += FunctionWhatsApp.postMensagem("553537150180", "Por favor informe seu CPF VALIDO - " + reserva.NomeHospede).Result;
+
+                            logSistema.Log += FunctionWhatsApp.postMensagem(reserva.ProxyCelular, "Por favor informe seu CPF VALIDO para reserva.").Result;
+                            logSistema.Log += FunctionWhatsApp.postMensagem(reserva.ProxyCelular, " Na recepçao e solicite para preencher no sistema.").Result;
+                            logSistema.Log += FunctionWhatsApp.postMensageFlowCPF(reserva.ProxyCelular).Result;
+                        }
+
                         if (voltar)
                             logSistema.Log += FunctionAPICLOUDBEDs.putReservation(reserva.IDReserva, "confirmed").Result;
                     }
@@ -824,6 +838,42 @@ namespace HoteldosNobresBlazor.Funcoes
             {
                 return e.Message + "\n";
             }
+        }
+
+        public static bool ValidarCPF(string cpf)
+        {
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+            int soma;
+            int resto;
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11)
+                return false;
+            tempCpf = cpf.Substring(0, 9);
+            soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return cpf.EndsWith(digito);
         }
     }
 }
