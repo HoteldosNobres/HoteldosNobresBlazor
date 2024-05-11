1c15d8cb5e6101edcacc9e425d8402f47bec43e7
Fabio Nobre <fabiohcnobre@hotmail.com>
2024-05-11 18:14:14 -0300

    #0 - Ajuste

---
+++ HoteldosNobresBlazor/HoteldosNobresBlazor/AddEndPointsSistema.cs
@@ -0,0 +1,204 @@
+﻿using HoteldosNobresBlazor.Funcoes;
+using HoteldosNobresBlazor.Services;
+using System.Text;
+
+namespace HoteldosNobresBlazor;
+ 
+public static class SistemaExtensions
+{
+    public static void AddEndPointsSisTema(this WebApplication app)
+    {
+        AppState sCOPP = app.Services.GetRequiredService<AppState>();
+        var cache = new CacheHotel(sCOPP);
+        cache.CacheExecutanado();
+
+        #region Endpoint Whatsapp
+        app.MapPost("/whatsapp", async (HttpContext httpContext) =>
+        {
+            try
+            {
+                using var reader = new StreamReader(httpContext.Request.Body, Encoding.UTF8);
+                string body = await reader.ReadToEndAsync();
+
+                //string verify_token = httpContext.Request.Query["hub.verify_token"];
+                //string challenge = httpContext.Request.Query["hub.challenge"];
+                //string mode = httpContext.Request.Query["hub.mode"];
+
+                sCOPP.MyMessageLogWhatsapp = " -Bodi-  " + body + " -Bodi-  ";
+                CacheHotel cache2 = new CacheHotel(sCOPP);
+                cache2.RecebeMensagem(body);
+
+                //sCOPP.MyMessage += verify_token + "<verify_token ";
+                //sCOPP.MyMessage += challenge + "<challenge ";
+                //sCOPP.MyMessage += mode + "<mode ";
+
+                //if (string.IsNullOrEmpty(verify_token))
+                //{
+                //    httpContext.Response.StatusCode = 400; // Bad Request
+                //    await httpContext.Response.WriteAsync("Token não fornecido.");
+                //    return;
+                //}
+                //bool isValid = verify_token.Replace("Bearer ", "") == "TOKENHOTELDOSNOBRES19";
+                //if (!isValid)
+                //{
+                //    httpContext.Response.StatusCode = 401; // Unauthorized
+                //    await httpContext.Response.WriteAsync("Token inválido.");
+                //    return;
+                //}
+
+                //sCOPP.MyMessage += token + " ";
+
+                httpContext.Response.StatusCode = 200;
+                //await httpContext.Response.WriteAsync(challenge);
+
+            }
+            catch (Exception ex)
+            {
+                await httpContext.Response.WriteAsync(ex.Message);
+                httpContext.Response.StatusCode = 500;
+            }
+
+        });
+
+        app.MapGet("/whatsapp", async (HttpContext httpContext) =>
+        {
+            try
+            {
+                //string token = httpContext.Request.Headers["Authorization"];
+                ////if (string.IsNullOrEmpty(token))
+                ////{
+                ////    httpContext.Response.StatusCode = 400; // Bad Request
+                ////    await httpContext.Response.WriteAsync("Token não fornecido.");
+                ////    return;
+                ////}
+
+                ////bool isValid = token.Replace("Bearer ", "") == "TOKENHOTELDOSNOBRES19";
+                ////if (!isValid)
+                ////{
+                ////    httpContext.Response.StatusCode = 401; // Unauthorized
+                ////    await httpContext.Response.WriteAsync("Token inválido.");
+                ////    return;
+                ////}
+                ///
+
+
+                using var reader = new StreamReader(httpContext.Request.Body, Encoding.UTF8);
+                string body = await reader.ReadToEndAsync();
+
+                string verify_token = httpContext.Request.Query["hub.verify_token"];
+                string challenge = httpContext.Request.Query["hub.challenge"];
+                string mode = httpContext.Request.Query["hub.mode"];
+
+                sCOPP.MyMessage += body + " ";
+                sCOPP.MyMessage += verify_token + " ";
+                sCOPP.MyMessage += challenge + " ";
+                sCOPP.MyMessage += mode + " ";
+
+
+                if (string.IsNullOrEmpty(verify_token))
+                {
+                    httpContext.Response.StatusCode = 400; // Bad Request
+                    await httpContext.Response.WriteAsync("Token não fornecido.");
+                    return;
+                }
+                bool isValid = verify_token.Replace("Bearer ", "") == "TOKENHOTELDOSNOBRES19";
+                if (!isValid)
+                {
+                    httpContext.Response.StatusCode = 401; // Unauthorized
+                    await httpContext.Response.WriteAsync("Token inválido.");
+                    return;
+                }
+
+
+                httpContext.Response.StatusCode = 200;
+                await httpContext.Response.WriteAsync(challenge);
+
+            }
+            catch (Exception ex)
+            {
+                await httpContext.Response.WriteAsync(ex.Message);
+                httpContext.Response.StatusCode = 500;
+            }
+
+        });
+
+        #endregion
+
+        app.MapPost("/addreserva", async (HttpContext httpContext) =>
+        {
+            try
+            {
+                using var reader = new StreamReader(httpContext.Request.Body, Encoding.UTF8);
+                string body = await reader.ReadToEndAsync();
+
+                CacheHotel cache2 = new CacheHotel(sCOPP);
+                await httpContext.Response.WriteAsync(cache2.CacheCreateReservationAsync(body).Result);
+
+            }
+            catch (Exception ex)
+            {
+                await httpContext.Response.WriteAsync(ex.Message);
+                httpContext.Response.StatusCode = 500;
+            }
+
+        });
+
+        app.MapPost("/status_changed", async (HttpContext httpContext) =>
+        {
+            try
+            {
+                using var reader = new StreamReader(httpContext.Request.Body, Encoding.UTF8);
+                string body = await reader.ReadToEndAsync();
+
+                CacheHotel cache2 = new CacheHotel(sCOPP);
+                await httpContext.Response.WriteAsync(cache2.CacheChangedStatus(body));
+
+            }
+            catch (Exception ex)
+            {
+                await httpContext.Response.WriteAsync(ex.Message);
+                httpContext.Response.StatusCode = 500;
+            }
+
+        });
+
+        app.MapPost("/accommodation_changed", async (HttpContext httpContext) =>
+        {
+            try
+            {
+                using var reader = new StreamReader(httpContext.Request.Body, Encoding.UTF8);
+                string body = await reader.ReadToEndAsync();
+
+                CacheHotel cache2 = new CacheHotel(sCOPP);
+                await httpContext.Response.WriteAsync(cache2.CacheAccommodation_changed(body));
+
+            }
+            catch (Exception ex)
+            {
+                await httpContext.Response.WriteAsync(ex.Message);
+                httpContext.Response.StatusCode = 500;
+            }
+
+        });
+
+        app.MapPost("/details_changed", async (HttpContext httpContext) =>
+        {
+            try
+            {
+                using var reader = new StreamReader(httpContext.Request.Body, Encoding.UTF8);
+                string body = await reader.ReadToEndAsync();
+
+                CacheHotel cache2 = new CacheHotel(sCOPP);
+                await httpContext.Response.WriteAsync(cache2.CacheDetails_changed(body));
+
+            }
+            catch (Exception ex)
+            {
+                await httpContext.Response.WriteAsync(ex.Message);
+                httpContext.Response.StatusCode = 500;
+            }
+
+        });
+
+    }
+}
--- HoteldosNobresBlazor/HoteldosNobresBlazor/Program.cs
+++ HoteldosNobresBlazor/HoteldosNobresBlazor/Program.cs
@@ -1,29 +1,23 @@
 using HoteldosNobresBlazor.Components;
-using HoteldosNobresBlazor.Funcoes;
 using HoteldosNobresBlazor.Services;
-using System.Text;
 using Blazored.LocalStorage;
-using KEYs = HoteldosNobresBlazor.Funcoes.KEYs; 
+using KEYs = HoteldosNobresBlazor.Funcoes.KEYs;
 using Microsoft.AspNetCore.Components.Authorization;
 using HoteldosNobresBlazor.Modelo;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using HoteldosNobresBlazor.Client.API;
 using MudBlazor.Services;
 using Microsoft.AspNetCore.Identity;
+using HoteldosNobresBlazor;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMudServices();
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
-
-//builder.Services.AddAuthorization();
 builder.Services.AddAuthorization(options =>
 {
 options.AddPolicy("Policy_Name", x => x.RequireRole("admin", "client") );
@@ -31,11 +25,8 @@ options.AddPolicy("Policy_Name", x => x.RequireRole("admin", "client") );
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddOptions();
 
-//builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
-//builder.Services.AddScoped<AuthenticationStateProvider, AuthAPI>();
-//builder.Services.AddScoped<AuthAPI>(sp => (AuthAPI)sp.GetRequiredService<AuthenticationStateProvider>()); 
 
 builder.Services.AddAuthentication().AddCookie();
 
@@ -58,19 +49,15 @@ builder.Services.AddHttpClient("CloudbedsAPI", client =>
 builder.Services.AddHttpClient();
 builder.Services.AddBlazorBootstrap();
 
-//builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<CookieHandler>();
 builder.Services.AddScoped<AppState>();  
 builder.Services.AddSingleton<AppState>();
 builder.Services.AddSingleton<IAuthorizationMiddlewareResultHandler, AuthorizationMiddlewareResultHandler>();
 
-
-//builder.Services.AddBlazoredLocalStorage();   // local storage
 builder.Services.AddBlazoredLocalStorage(config => config.JsonSerializerOptions.WriteIndented = true);  // local storage
  
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging(); 
@@ -89,205 +76,12 @@ app.UseStaticFiles();
 app.UseAuthentication();
 app.UseAuthorization();
 
-
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(HoteldosNobresBlazor.Client._Imports).Assembly);
-
-
-AppState sCOPP = app.Services.GetRequiredService<AppState>();
-
-
-app.MapPost("/addreserva", async (HttpContext httpContext) =>
-{
-    try
-    {
-        using var reader = new StreamReader(httpContext.Request.Body, Encoding.UTF8);
-        string body = await reader.ReadToEndAsync();
-
-        CacheHotel cache2 = new CacheHotel(sCOPP);  
-        await httpContext.Response.WriteAsync(cache2.CacheCreateReservationAsync(body).Result); 
-          
-    }
-    catch (Exception ex)
-    {
-        await httpContext.Response.WriteAsync(ex.Message);
-        httpContext.Response.StatusCode = 500;
-    }
-
-});
-
-app.MapPost("/status_changed", async (HttpContext httpContext) =>
-    {
-        try
-        {
-            using var reader = new StreamReader(httpContext.Request.Body, Encoding.UTF8);
-            string body = await reader.ReadToEndAsync();
-
-            CacheHotel cache2 = new CacheHotel(sCOPP); 
-            await httpContext.Response.WriteAsync(cache2.CacheChangedStatus(body));
-
-        }
-        catch (Exception ex)
-        {
-            await httpContext.Response.WriteAsync(ex.Message);
-            httpContext.Response.StatusCode = 500;
-        }
-
-    });
-
-app.MapPost("/accommodation_changed", async (HttpContext httpContext) =>
-{
-    try
-    {
-        using var reader = new StreamReader(httpContext.Request.Body, Encoding.UTF8);
-        string body = await reader.ReadToEndAsync();
-
-        CacheHotel cache2 = new CacheHotel(sCOPP);
-        await httpContext.Response.WriteAsync(cache2.CacheAccommodation_changed(body));
-
-    }
-    catch (Exception ex)
-    {
-        await httpContext.Response.WriteAsync(ex.Message);
-        httpContext.Response.StatusCode = 500;
-    }
-
-});
-
-app.MapPost("/details_changed", async (HttpContext httpContext) =>
-{
-    try
-    {
-        using var reader = new StreamReader(httpContext.Request.Body, Encoding.UTF8);
-        string body = await reader.ReadToEndAsync();
-
-        CacheHotel cache2 = new CacheHotel(sCOPP);
-        await httpContext.Response.WriteAsync(cache2.CacheDetails_changed(body));
-
-    }
-    catch (Exception ex)
-    {
-        await httpContext.Response.WriteAsync(ex.Message);
-        httpContext.Response.StatusCode = 500;
-    }
-
-});
-
-app.MapPost("/whatsapp", async (HttpContext httpContext) =>
-{
-    try
-    {
-        using var reader = new StreamReader(httpContext.Request.Body, Encoding.UTF8);
-        string body = await reader.ReadToEndAsync();
-
-        //string verify_token = httpContext.Request.Query["hub.verify_token"];
-        //string challenge = httpContext.Request.Query["hub.challenge"];
-        //string mode = httpContext.Request.Query["hub.mode"];
-
-        sCOPP.MyMessageLogWhatsapp = " -Bodi-  " + body + " -Bodi-  ";
-        CacheHotel cache2 = new CacheHotel(sCOPP);
-        cache2.RecebeMensagem(body);
-
-        //sCOPP.MyMessage += verify_token + "<verify_token ";
-        //sCOPP.MyMessage += challenge + "<challenge ";
-        //sCOPP.MyMessage += mode + "<mode ";
-
-        //if (string.IsNullOrEmpty(verify_token))
-        //{
-        //    httpContext.Response.StatusCode = 400; // Bad Request
-        //    await httpContext.Response.WriteAsync("Token não fornecido.");
-        //    return;
-        //}
-        //bool isValid = verify_token.Replace("Bearer ", "") == "TOKENHOTELDOSNOBRES19";
-        //if (!isValid)
-        //{
-        //    httpContext.Response.StatusCode = 401; // Unauthorized
-        //    await httpContext.Response.WriteAsync("Token inválido.");
-        //    return;
-        //}
-
-        //sCOPP.MyMessage += token + " ";
-
-        httpContext.Response.StatusCode = 200;
-        //await httpContext.Response.WriteAsync(challenge);
-
-    }
-    catch (Exception ex)
-    {
-        await httpContext.Response.WriteAsync(ex.Message);
-        httpContext.Response.StatusCode = 500;
-    }
-
-});
-
-app.MapGet("/whatsapp", async (HttpContext httpContext) =>
-{
-    try
-    {
-        //string token = httpContext.Request.Headers["Authorization"];
-        ////if (string.IsNullOrEmpty(token))
-        ////{
-        ////    httpContext.Response.StatusCode = 400; // Bad Request
-        ////    await httpContext.Response.WriteAsync("Token não fornecido.");
-        ////    return;
-        ////}
-
-        ////bool isValid = token.Replace("Bearer ", "") == "TOKENHOTELDOSNOBRES19";
-        ////if (!isValid)
-        ////{
-        ////    httpContext.Response.StatusCode = 401; // Unauthorized
-        ////    await httpContext.Response.WriteAsync("Token inválido.");
-        ////    return;
-        ////}
-        ///
-
-
-        using var reader = new StreamReader(httpContext.Request.Body, Encoding.UTF8);
-        string body = await reader.ReadToEndAsync();
-
-        string verify_token = httpContext.Request.Query["hub.verify_token"];
-        string challenge = httpContext.Request.Query["hub.challenge"];
-        string mode = httpContext.Request.Query["hub.mode"];
-
-        sCOPP.MyMessage += body + " ";
-        sCOPP.MyMessage += verify_token + " ";
-        sCOPP.MyMessage += challenge + " ";
-        sCOPP.MyMessage += mode + " ";
-
-
-        if (string.IsNullOrEmpty(verify_token))
-        {
-            httpContext.Response.StatusCode = 400; // Bad Request
-            await httpContext.Response.WriteAsync("Token não fornecido.");
-            return;
-        }
-        bool isValid = verify_token.Replace("Bearer ", "") == "TOKENHOTELDOSNOBRES19";
-        if (!isValid)
-        {
-            httpContext.Response.StatusCode = 401; // Unauthorized
-            await httpContext.Response.WriteAsync("Token inválido.");
-            return;
-        }
-
-
-        httpContext.Response.StatusCode = 200;
-        await httpContext.Response.WriteAsync(challenge);
-
-    }
-    catch (Exception ex)
-    {
-        await httpContext.Response.WriteAsync(ex.Message);
-        httpContext.Response.StatusCode = 500;
-    }
-
-});
-
-var cache = new CacheHotel(sCOPP);
-
-cache.CacheExecutanado();
  
-
+app.AddEndPointsSisTema();
+ 
 app.Run();
 
