dc29caf83aeb6dda9dd66fae5a4665ff8ccee4ac
Fabio Nobre <fabiohcnobre@hotmail.com>
2024-12-19 20:01:27 -0300

    #0

--- HoteldosNobresBlazor/HoteldosNobresBlazor.Client/Funcoes/FunctionGoogle.cs
+++ HoteldosNobresBlazor/HoteldosNobresBlazor.Client/Funcoes/FunctionGoogle.cs
@@ -10,19 +10,19 @@ namespace HoteldosNobresBlazor.FuncoesClient
 {
     public class FunctionGoogle
     {
-        static string[] Scopes = { PeopleServiceService.Scope.Contacts  }; 
+        static string[] Scopes = { PeopleServiceService.Scope.Contacts };
 
         public FunctionGoogle() { }
-        
+
         public static UserCredential? UserCredential()
         {
             try
-            { 
+            {
                 ClientSecrets secrets = new ClientSecrets()
                 {
                     ClientId = KEYs.ClientId_GOOGLE,
                     ClientSecret = KEYs.ClientSecret_GOOGLE
-                }; 
+                };
                 var token = new TokenResponse { RefreshToken = KEYs.RefreshToken_GOOGLE };
 
                 UserCredential credential = new UserCredential(new GoogleAuthorizationCodeFlow(
@@ -32,78 +32,56 @@ namespace HoteldosNobresBlazor.FuncoesClient
                     }),
                     "user",
                     token);
-                 
 
                 return credential;
             }
-            catch (FileNotFoundException e)
-            {
-                Console.WriteLine(e.Message);
-                return null;
-            }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
-                return null;
+                throw new Exception("Credencial: " + e.Message);
             }
 
         }
 
         public static async Task<string> AddPeopleAsync(string nome, string local, string telefone, string email)
-        { 
-            try
+        {
+            UserCredential credential = UserCredential();
+            var service = new PeopleServiceService(new BaseClientService.Initializer()
             {
-                UserCredential credential = UserCredential();
-                string givenome = nome;
-                if (!string.IsNullOrEmpty(local))
-                    givenome = nome + " - " + local;
+                HttpClientInitializer = credential,
+                ApplicationName = "APP_NAME",
+            });
+             
+            string givenome = nome;
+            if (!string.IsNullOrEmpty(local))
+                givenome = nome + " - " + local;
 
-                var service = new PeopleServiceService(new BaseClientService.Initializer()
-                {
-                    HttpClientInitializer = credential,
-                    ApplicationName = "APP_NAME",
-                });
-
-                Person contactToCreate = new Person();
-                List<Name> names = new List<Name>();
-                names.Add(new Name()
-                {
-                    GivenName = givenome,
-                });
-                contactToCreate.Names = names;
-
-                List<PhoneNumber> phones = new List<PhoneNumber>();
-                phones.Add(new PhoneNumber()
-                {
-                    Value = "55" + telefone.Replace("+55", "").Replace(" ", ""),
-                });
-                contactToCreate.PhoneNumbers = phones;
-
-                if (email is not null && email != "" && email != "email")
-                {
-                    List<EmailAddress> emails = new List<EmailAddress>();
-                    emails.Add(new EmailAddress()
-                    {
-                        Value = email,
-                    });
-                    contactToCreate.EmailAddresses = emails;
-                }
-                 
-                var request = service.People.CreateContact(contactToCreate);
-                 
-                Person createdContact = await request.ExecuteAsync();
-
-                return "OK";
-
-            }
-            catch (Exception e)
+            Person contactToCreate = new Person();
+            List<Name> names = new List<Name>();
+            names.Add(new Name()
             {
-                Console.WriteLine(e.Message);
-                if (e.InnerException != null)
-                    return "ERRO: " + e.Message + " - " + e.InnerException.Message;
-                else
-                    return "ERRO: " + e.Message;
-            }
+                GivenName = givenome,
+            });
+            contactToCreate.Names = names;
+
+            List<PhoneNumber> phones = new List<PhoneNumber>();
+            phones.Add(new PhoneNumber()
+            {
+                Value = "55" + telefone.Replace("+55", "").Replace(" ", "").Replace("-", ""),
+            });
+            contactToCreate.PhoneNumbers = phones;
+             
+            List<EmailAddress> emails = new List<EmailAddress>();
+            emails.Add(new EmailAddress()
+            {
+                Value = email,
+            });
+            contactToCreate.EmailAddresses = emails;
+             
+            var request = service.People.CreateContact(contactToCreate);
+
+            var createdContact = await request.ExecuteAsync();
+
+            return "OK";
         }
     }
 }
